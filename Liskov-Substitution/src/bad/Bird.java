d4c29d099d55ac79f16b99fbacd352b67567671d
ApsaraWitharana <asachini095@gmail.com>
2025-02-06 18:40:17 +0530

    add-solid-principle-liskov-substitution

---
+++ Liskov-Substitution/src/bad/Bird.java
@@ -0,0 +1,20 @@
+package bad;
+
+
+public abstract class Bird {
+    protected abstract void eat();
+
+    abstract void fly();
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Liskov-Substitution/src/good/Penguin.java
@@ -0,0 +1,9 @@
+package good;
+
+
+public class Penguin extends Bird {
+    @Override
+    public void eat() {
+        System.out.println("Penguin can fly");
+    }
+}
